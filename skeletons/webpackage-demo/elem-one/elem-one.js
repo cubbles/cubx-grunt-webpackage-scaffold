80d45e2f4ea8e75b50747c640fdf336e774e33e3
Judit Ross <jtrs@incowia.com>
2016-09-02 15:50:06 +0200

    PLAT-566: fixed eslint issues

--- skeletons/webpackage-demo/elem-one/elem-one.js
+++ skeletons/webpackage-demo/elem-one/elem-one.js
@@ -1,40 +1,40 @@
-(function() {
-    'use strict';
+(function () {
+  'use strict';
+  /**
+   * Get help:
+   * > Lifecycle callbacks:
+   * https://www.polymer-project.org/1.0/docs/devguide/registering-elements.html#lifecycle-callbacks
+   *
+   * Access the Cubbles-Component-Model:
+   * > Access slot values:
+   * slot 'a': this.getA(); | this.setA(value)
+   */
+  CubxPolymer({
+    is: 'elem-one',
+
     /**
-     * Get help:
-     * > Lifecycle callbacks:
-     * https://www.polymer-project.org/1.0/docs/devguide/registering-elements.html#lifecycle-callbacks
-     *
-     * Access the Cubbles-Component-Model:
-     * > Access slot values:
-     * slot 'a': this.getA(); | this.setA(value)
+     * Manipulate an element’s local DOM when the element is constructed.
      */
-    CubxPolymer({
-        is: 'elem-one',
+    ready: function () {
+      // set value-attribute of element with id='slota' to the initial value of slot 'a'
+      this.$.slota.setAttribute('value', this.getA());
+    },
 
-        /**
-         * Manipulate an element’s local DOM when the element is constructed.
-         */
-        ready: function() {
-            // set value-attribute of element with id='slota' to the initial value of slot 'a'
-            this.$.slota.setAttribute('value', this.getA());
-        },
+    /**
+     * A handler to be called by a dom-element
+     * @param {event} event
+     */
+    inputFieldSlotAChanged: function (event) {
+      // update the cubbles-model
+      this.setA(event.target.value);
+    },
 
-        /**
-         * A handler to be called by a dom-element
-         * @param {event} event
-         */
-        inputFieldSlotAChanged: function(event) {
-            // update the cubbles-model
-            this.setA(event.target.value);
-        },
-
-        /**
-         *  Observe the Cubbles-Component-Model: If value for slot 'a' has changed ...
-         */
-        modelAChanged: function(newValue) {
-            // update the view
-            this.$.slota.setAttribute('value', newValue);
-        }
-    });
+    /**
+     *  Observe the Cubbles-Component-Model: If value for slot 'a' has changed ...
+     */
+    modelAChanged: function (newValue) {
+      // update the view
+      this.$.slota.setAttribute('value', newValue);
+    }
+  });
 }());
