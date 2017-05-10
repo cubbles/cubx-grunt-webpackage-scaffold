2d64f7969134162bcc81cfe23b6c1551ba04b9e5
Judit Ross <jtrs@incowia.com>
2017-05-10 09:00:36 +0200

    CDT-54: RTE version updated to 2.3.0

--- tasks/cubx_elementary_scaffold.js
+++ tasks/cubx_elementary_scaffold.js
@@ -28,7 +28,7 @@ module.exports = function (grunt) {
           ],
           dependencies: [
             {
-              webpackageId: 'cubx.core.rte@2.2.3',
+              webpackageId: 'cubx.core.rte@2.3.0',
               artifactId: 'cubxpolymer'
             }
           ],
