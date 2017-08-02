59fd5f26c18b569683e7d5986d2ef0294900b931
Judit Ross <jtrs@incowia.com>
2017-08-02 13:52:08 +0200

    CDT-65: use rte version 2.4.0

--- tasks/cubx_elementary_scaffold.js
+++ tasks/cubx_elementary_scaffold.js
@@ -28,7 +28,7 @@ module.exports = function (grunt) {
           ],
           dependencies: [
             {
-              webpackageId: 'cubx.core.rte@2.3.1',
+              webpackageId: 'cubx.core.rte@2.4.0',
               artifactId: 'cubxpolymer'
             }
           ],
