38832477d62a25af8fffe835294603a9bb413519
Judit Ross <jtrs@incowia.com>
2018-07-10 14:45:21 +0200

    cubx-grunt-webpackage-scaffold#10 Update for cubx.core.rte@2.5.1

--- tasks/cubx_elementary_scaffold.js
+++ tasks/cubx_elementary_scaffold.js
@@ -29,7 +29,7 @@ module.exports = function (grunt) {
           ],
           dependencies: [
             {
-              webpackageId: 'cubx.core.rte@2.5.0',
+              webpackageId: 'cubx.core.rte@2.5.1',
               artifactId: 'cubxpolymer'
             }
           ],
